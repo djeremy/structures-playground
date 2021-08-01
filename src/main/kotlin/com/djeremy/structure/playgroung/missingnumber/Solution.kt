bd25f1589723f482c85a1e09b48104fe2c6aa733
djeremy <bugai.yevgenii@gmail.com>
2021-08-01 22:25:01 +0200

    Supply missingnumber solution with a description

--- src/main/kotlin/com/djeremy/structure/playgroung/missingnumber/Solution.kt
+++ src/main/kotlin/com/djeremy/structure/playgroung/missingnumber/Solution.kt
@@ -1,7 +1,9 @@
 package com.djeremy.structure.playgroung.missingnumber
 
+// You are given an array of positive numbers from 1 to n, such that
+// all numbers from 1 to n are present except one number x.
+// You have to find x. The input array is not sorted.
 fun main() {
-
     while (true) {
         val input = readLine()!!.split(' ').map { it.toInt() }
 
